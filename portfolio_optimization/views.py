33a1169ca2cf70898c3faee33b9079a29da06445
Marcelo Barrero <marcelo.barrero@live.com>
2024-05-01 22:24:00 -0400

    search bar htmx for symbol company_name and index

--- portfolio_optimization/views.py
+++ portfolio_optimization/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from portfolio_optimization.models import Ticker
+from django.db.models import Q
 
 # Create your views here.
 
@@ -12,6 +13,10 @@ class HomePageView(TemplateView):
 def search_ticker(request):
     search_text = request.POST.get("search")
 
-    results = Ticker.objects.filter(symbol__icontains=search_text)
+    results = Ticker.objects.filter(
+        Q(symbol__icontains=search_text)
+        | Q(company_name__icontains=search_text)
+        | Q(index__icontains=search_text)
+    )
     context = {"results": results}
     return render(request, "partials/search-results.html", context)
