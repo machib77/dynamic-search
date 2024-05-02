2496bb10a5e77c3ec7b24ca457fe7b47fbd7593d
Marcelo Barrero <marcelo.barrero@live.com>
2024-05-01 21:47:15 -0400

    search bar htmx init

--- portfolio_optimization/urls.py
+++ portfolio_optimization/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import HomePageView
+from portfolio_optimization import views
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
 ]
+
+htmx_urlpatterns = [path("search-ticker/", views.search_ticker, name="search-ticker")]
+
+urlpatterns += htmx_urlpatterns
--- portfolio_optimization/views.py
+++ portfolio_optimization/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from portfolio_optimization.models import Ticker
 
 # Create your views here.
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+def search_ticker(request):
+    search_text = request.POST.get("search")
+
+    results = Ticker.objects.filter(symbol__icontains=search_text)
+    context = {"results": results}
+    return render(request, "partials/search-results.html", context)
